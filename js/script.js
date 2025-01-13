b4d147faa6725992e57924039221084ab2393f50
mdgamer <mohammadsaqulain@gmail.com>
2025-01-13 23:44:40 +0530

    Adding information and navigation active changes

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ var typed = new Typed(".typing",{
     loop:true
 })
 
+const navActiveSwitcher =  document.querySelectorAll('.nav a');
+
+navActiveSwitcher.forEach(link => {
+    link.addEventListener('click', function () {
+      // Remove the 'active' class from all links
+      navActiveSwitcher.forEach(link => link.classList.remove('active'));
+      
+      // Add the 'active' class to the clicked link
+      this.classList.add('active');
+    });
+  });
+
